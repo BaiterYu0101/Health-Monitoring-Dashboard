37e3ed397741e3d1a9b2824d6e0e5b7f51c4d2e3
CheeXiangLing <143257885+CheeXiangLing@users.noreply.github.com>
2024-09-23 21:55:49 +0800

    Update optimization.py

--- core/optimization.py
+++ core/optimization.py
@@ -1,12 +1,14 @@
 # core/optimization.py
 
 class OptimizationAdvisor:
-    def get_cpu_advice(self, cpu_usage):
-        if cpu_usage > 80:
-            return "High CPU usage detected. Close unnecessary applications."
-        return "CPU usage is normal."
-
-    def get_ram_advice(self, ram_usage):
-        if ram_usage > 80:
-            return "High RAM usage detected. Consider closing some programs."
-        return "RAM usage is normal."
+    
+    def get_cpu_ram_advice(self, cpu_usage, ram_usage,disk_usage):
+        match(cpu_usage, ram_usage,disk_usage):
+            case(cpu, _, _) if cpu > 5:
+                return "High CPU usage detected. Close unnecessary applications."
+            case(_,ram, _) if ram > 90:
+                return "High RAM usage detected. Consider closing some programs."
+            case( _, _,disk) if disk > 90:
+                return "The disk is running low on available storage."
+            case  _:
+                return "All are normal"
